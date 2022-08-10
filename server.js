50d38b8e1096fc18f315abe9cd200f0f48a3c914
Kammin Avery <kammin.avery@gmail.com>
2022-08-09 22:17:33 -0600

    got sequelize up and running

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('sequelize');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send ('hello world!'))
+// app.get('/', (req, res) => res.send ('hello world!'))
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(routes);
